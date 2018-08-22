068b1c43f6a189eb26f0ba30dfd01a235d14776b
vdavid30 <spm1dh@gmail.com>
2018-08-22 13:30:10 -0500

    finshed

---
+++ src/main/java/com/eci/cosw/newdemo/NewDemoApplication.java
@@ -0,0 +1,12 @@
+package com.eci.cosw.newdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/eci/cosw/newdemo/NewDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eci.cosw.newdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
